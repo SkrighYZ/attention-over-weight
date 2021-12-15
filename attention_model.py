7e298c677f49df59b95b23afa6edc5e8af6c586d
SkrighYZ <zhang.yipeng@columbia.edu>
2021-12-15 01:00:02 -0500

    debug

--- attention_model.py
+++ attention_model.py
@@ -82,7 +82,7 @@ class MaskedConv2d(nn.Module):
         w = self.weight
         masked_w = self.attns[task](x, w)
 
-        if masked_w.size().size(0) > 1:
+        if masked_w.ndim > 1:
             maked_w = masked_w.view(batch_size, self.out_channels, self.in_channels, *self.kernel_size)
 
             # move batch dim into out_channels
