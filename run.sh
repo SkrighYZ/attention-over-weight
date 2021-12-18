dbd9d07ca63ad9a3f00784b65677708391597915
SkrighYZ <zhang.yipeng@columbia.edu>
2021-12-18 15:36:23 -0500

    update training script

--- run.sh
+++ run.sh
@@ -2,11 +2,14 @@
 
 # python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 8
 # python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
-# python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 2
 # python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 1
-# python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 2 --res 
+
+python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4 --res 
 
 
+#########################
+
+# python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 2
 python train_new_task_aow.py --dataset svhn --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
 python train_new_task_aow.py --dataset gtsrb --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
 
@@ -17,11 +20,6 @@ python train_new_task_aow.py --dataset vgg-flowers --wd 2e-3 --source ../models/
 python train_new_task_aow.py --dataset omniglot --wd 5e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
 python train_new_task_aow.py --dataset ucf101 --wd 5e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
 python train_new_task_aow.py --dataset daimlerpedcls --wd 5e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4
-
-
-
-#########################
-python train_new_task_aow.py --dataset cifar100 --wd 1e-4 --source ../models/pretrained/imnet.t7 --expdir ../results --batch_size 128 --att_factor 4 --res 
 
 
 #########################
